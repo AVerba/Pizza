15f562358a6b624e2aff47d7f0f89eeeaedfd514
Oleksii Verbytskyi <alverba88@gmail.com>
2023-05-10 14:20:34 +0300

    modyfy menu and logo component

--- src/modules/common/components/UI/MenuLink/MenuLink.ui.jsx
+++ src/modules/common/components/UI/MenuLink/MenuLink.ui.jsx
@@ -2,7 +2,6 @@ import styles from "./MenuLink.moddule.scss";
 import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
 import cx from "classnames";
-import { Container } from "../../Container";
 
 export const MenuLink = ({ path, className, activeClassName, title, children }) => {
   const classList = cx(styles.link, className);
